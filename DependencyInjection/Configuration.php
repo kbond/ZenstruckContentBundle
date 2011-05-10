93af828a26811b2e17d6b54bc8b766373172b4c6
kbond <kevinbond@gmail.com>
2011-05-10 17:50:55 -0400

    added configuration class

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,21 +10,14 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('zenstruck_version');
+        $rootNode = $treeBuilder->root('zenstruck_cms');
 
-        $rootNode
+        $rootNode            
             ->children()
-                ->booleanNode('enabled')->defaultFalse()->end()
-                ->node('version', 'variable')->defaultFalse()->end()
-                ->node('file', 'variable')->defaultValue('%kernel.root_dir%/../VERSION')->end()
-                ->node('text', 'variable')->defaultFalse()->end()
-                ->booleanNode('toolbar')->defaultFalse()->end()
-                ->arrayNode('block')
-                    ->children()
-                        ->booleanNode('enabled')->defaultFalse()->end()
-                        ->node('position', 'variable')->defaultValue('vb-bottom-right')->end()
-                        ->node('prefix', 'variable')->defaultValue('Version: ')->end()
-                    ->end()
+                ->arrayNode('content_types')                    
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                ->end()
             ->end()
         ;
 
--- DependencyInjection/ZenstruckCMSExtension.php
+++ DependencyInjection/ZenstruckCMSExtension.php
@@ -6,14 +6,22 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
 
-class CMSExtension extends Extension
+class ZenstruckCMSExtension extends Extension
 {
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
+        $configuration = new Configuration();
+        
+        $config = $processor->processConfiguration($configuration, $configs);
+        
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('listener.xml');
+        
+        var_dump($config);
     }
 
 }
