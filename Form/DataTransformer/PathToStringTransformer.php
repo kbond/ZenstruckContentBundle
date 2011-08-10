hatimeria/ZenstruckCMSBundle

Added PathUniqueValidator, NodeBaseFormType for some base work with forms

Form/DataTransformer/PathToStringTransformer.php
@@ -1,0 +1,65 @@
+<?php
+
+namespace Zenstruck\Bundle\CMSBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+use Zenstruck\Bundle\CMSBundle\Entity\Path;
+
+class PathToStringTransformer implements DataTransformerInterface
+{
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    protected $repository;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->repository = $em->getRepository('ZenstruckCMSBundle:Path');
+    }
+
+    /**
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @param \Zenstruck\Bundle\CMSBundle\Entity\Path $value
+     * @return null|string
+     */
+    function transform($value)
+    {
+        if (null === $value) {
+            return null;
+        }
+        if (!$value instanceof Path) {
+            throw new UnexpectedTypeException($value, 'Zenstruck\Bundle\CMSBundle\Entity\Path');
+        }
+
+        return $value->getUri();
+    }
+
+    /**
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @param string $value
+     * @return null|\Zenstruck\Bundle\CMSBundle\Entity\Path
+     */
+    function reverseTransform($value)
+    {
+        if (null === $value) {
+            return null;
+        }
+        if (!is_string($value)) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+        
+        $object = $this->repository->findOneBy(array('uri' => $value));
+        if (null === $object) {
+            $object = new Path($value);
+        }
+
+        return $object;
+    }
+
+}

Form/Type/PathFormType.php
@@ -1,0 +1,38 @@
+<?php
+
+namespace Zenstruck\Bundle\CMSBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+use Zenstruck\Bundle\CMSBundle\Form\DataTransformer\PathToStringTransformer;
+
+class PathFormType extends AbstractType
+{
+    /**
+     * @var \Zenstruck\Bundle\CMSBundle\Form\DataTransformer\PathToStringTransformer
+     */
+    protected $pathToStringTransformer;
+
+    public function __construct(PathToStringTransformer $pathToStringTransformer)
+    {
+        $this->pathToStringTransformer = $pathToStringTransformer;
+    }
+
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->appendClientTransformer($this->pathToStringTransformer);
+    }
+
+    public function getParent(array $options)
+    {
+        return 'text';
+    }
+
+    public function getName()
+    {
+        return 'zenstruck_cms_path';
+    }
+}

Listener/Form/FormListener.php
@@ -1,0 +1,29 @@
+<?php
+
+namespace Zenstruck\Bundle\CMSBundle\Listener\Form;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Event\FilterDataEvent;
+
+use Zenstruck\Bundle\CMSBundle\Entity\Node;
+
+class FormListener implements EventSubscriberInterface
+{
+    public function onBindClientData(FilterDataEvent $event)
+    {
+        /* @var \Zenstruck\Bundle\CMSBundle\Entity\Node $node */
+        $node = $event->getForm()->getClientData();
+
+        if (is_object($node)) {
+            $node->setOldPrimaryPath($node->getPrimaryPath());
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::BIND_CLIENT_DATA => 'onBindClientData',
+        );
+    }
+}

Repository/NodeRepository.php
@@ -2,8 +2,9 @@
 
 namespace Zenstruck\Bundle\CMSBundle\Repository;
 
-use Zenstruck\ZenstruckBundle\Entity\Project;
 use Doctrine\ORM\EntityRepository;
+
+use Zenstruck\Bundle\CMSBundle\Entity\Path;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
@@ -20,6 +21,19 @@ class NodeRepository extends EntityRepository
                 ->from('ZenstruckCMSBundle:Node', 'n')
                 ->leftJoin('n.primaryPath', 'p')
                 ->where('p.uri = :uri')->setParameter('uri', $uri)
+                ->getQuery();
+
+        return $query->getOneOrNullResult();
+    }
+
+    public function findOneByPath(Path $path)
+    {
+        $qb = $this->_em->createQueryBuilder();
+
+        $query = $qb
+                ->select('n')
+                ->from('ZenstruckCMSBundle:Node', 'n')
+                ->where('n.primaryPath = :nodeId')->setParameter('nodeId', $path->getId())
                 ->getQuery();
 
         return $query->getOneOrNullResult();

Validator/PathUnique.php
@@ -1,0 +1,24 @@
+<?php
+
+namespace Zenstruck\Bundle\CMSBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class PathUnique extends Constraint
+{
+    public $message = 'Path already exists.';
+
+    public function validatedBy()
+    {
+        return 'zenstruck_cms.validator.path_unique';
+    }
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+
+}

Validator/PathUniqueValidator.php
@@ -1,0 +1,68 @@
+<?php
+
+namespace Zenstruck\Bundle\CMSBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+use Doctrine\ORM\EntityManager;
+
+class PathUniqueValidator extends ConstraintValidator
+{
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    protected $pathRepository;
+    /**
+     * @var \Zenstruck\Bundle\CMSBundle\Repository\NodeRepository
+     */
+    protected $nodeRepository;
+
+    /**
+     * @param \Doctrine\ORM\EntityRepository $repository
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->pathRepository = $em->getRepository('ZenstruckCMSBundle:Path');
+        $this->nodeRepository = $em->getRepository('ZenstruckCMSBundle:Node');
+    }
+
+    /**
+     * @param \Zenstruck\Bundle\CMSBundle\Entity\Node $value
+     * @param \Symfony\Component\Validator\Constraint $constraint
+     * @return bool
+     */
+    public function isValid($value, Constraint $constraint)
+    {
+        $path = $value->getPrimaryPath();
+        if (null === $path) {
+
+            return true;
+        }
+        $conflicts = $this->pathRepository->findBy(array('uri' => $path->getUri()));
+        if (empty($conflicts)) {
+
+            return true;
+        }
+
+        /* @var \Zenstruck\Bundle\CMSBundle\Entity\Path $old */
+        $old = $value->getOldPrimaryPath();
+        if (!is_object($old)) {
+            $this->setMessage($constraint->message);
+            
+            return false;
+        }
+
+        foreach ($conflicts as $conflict) {
+            /* @var \Zenstruck\Bundle\CMSBundle\Entity\Path $conflict */
+            if (($conflict->getId() != $old->getId()) && $this->nodeRepository->findOneByPath($conflict)) {
+                $this->setMessage($constraint->message);
+                
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+}

kbond/ZenstruckContentBundle

Form/DataTransformer/PathToStringTransformer.php
@@ -1,0 +1,65 @@
+<?php
+
+namespace Zenstruck\Bundle\ContentBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+use Zenstruck\Bundle\ContentBundle\Entity\Path;
+
+class PathToStringTransformer implements DataTransformerInterface
+{
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    protected $repository;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->repository = $em->getRepository('ZenstruckContentBundle:Path');
+    }
+
+    /**
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @param \Zenstruck\Bundle\ContentBundle\Entity\Path $value
+     * @return null|string
+     */
+    function transform($value)
+    {
+        if (null === $value) {
+            return null;
+        }
+        if (!$value instanceof Path) {
+            throw new UnexpectedTypeException($value, 'Zenstruck\Bundle\ContentBundle\Entity\Path');
+        }
+
+        return $value->getUri();
+    }
+
+    /**
+     * @throws \Symfony\Component\Form\Exception\UnexpectedTypeException
+     * @param string $value
+     * @return null|\Zenstruck\Bundle\ContentBundle\Entity\Path
+     */
+    function reverseTransform($value)
+    {
+        if (null === $value) {
+            return null;
+        }
+        if (!is_string($value)) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+        
+        $object = $this->repository->findOneBy(array('uri' => $value));
+        if (null === $object) {
+            $object = new Path($value);
+        }
+
+        return $object;
+    }
+
+}

Form/Type/PathFormType.php
@@ -1,0 +1,38 @@
+<?php
+
+namespace Zenstruck\Bundle\ContentBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+use Zenstruck\Bundle\ContentBundle\Form\DataTransformer\PathToStringTransformer;
+
+class PathFormType extends AbstractType
+{
+    /**
+     * @var \Zenstruck\Bundle\ContentBundle\Form\DataTransformer\PathToStringTransformer
+     */
+    protected $pathToStringTransformer;
+
+    public function __construct(PathToStringTransformer $pathToStringTransformer)
+    {
+        $this->pathToStringTransformer = $pathToStringTransformer;
+    }
+
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->appendClientTransformer($this->pathToStringTransformer);
+    }
+
+    public function getParent(array $options)
+    {
+        return 'text';
+    }
+
+    public function getName()
+    {
+        return 'zenstruck_content_path';
+    }
+}

Listener/Form/FormListener.php
@@ -1,0 +1,29 @@
+<?php
+
+namespace Zenstruck\Bundle\ContentBundle\Listener\Form;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Event\FilterDataEvent;
+
+use Zenstruck\Bundle\ContentBundle\Entity\Node;
+
+class FormListener implements EventSubscriberInterface
+{
+    public function onBindClientData(FilterDataEvent $event)
+    {
+        /* @var \Zenstruck\Bundle\ContentBundle\Entity\Node $node */
+        $node = $event->getForm()->getClientData();
+
+        if (is_object($node)) {
+            $node->setOldPrimaryPath($node->getPrimaryPath());
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::BIND_CLIENT_DATA => 'onBindClientData',
+        );
+    }
+}

Repository/NodeRepository.php
@@ -2,8 +2,9 @@
 
 namespace Zenstruck\Bundle\ContentBundle\Repository;
 
-use Zenstruck\ZenstruckBundle\Entity\Project;
 use Doctrine\ORM\EntityRepository;
+
+use Zenstruck\Bundle\ContentBundle\Entity\Path;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
@@ -20,6 +21,19 @@ class NodeRepository extends EntityRepository
                 ->from('ZenstruckContentBundle:Node', 'n')
                 ->leftJoin('n.primaryPath', 'p')
                 ->where('p.uri = :uri')->setParameter('uri', $uri)
+                ->getQuery();
+
+        return $query->getOneOrNullResult();
+    }
+
+    public function findOneByPath(Path $path)
+    {
+        $qb = $this->_em->createQueryBuilder();
+
+        $query = $qb
+                ->select('n')
+                ->from('ZenstruckContentBundle:Node', 'n')
+                ->where('n.primaryPath = :nodeId')->setParameter('nodeId', $path->getId())
                 ->getQuery();
 
         return $query->getOneOrNullResult();

Validator/PathUnique.php
@@ -1,0 +1,24 @@
+<?php
+
+namespace Zenstruck\Bundle\ContentBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class PathUnique extends Constraint
+{
+    public $message = 'Path already exists.';
+
+    public function validatedBy()
+    {
+        return 'zenstruck_content.validator.path_unique';
+    }
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+
+}

Validator/PathUniqueValidator.php
@@ -1,0 +1,68 @@
+<?php
+
+namespace Zenstruck\Bundle\ContentBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+use Doctrine\ORM\EntityManager;
+
+class PathUniqueValidator extends ConstraintValidator
+{
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    protected $pathRepository;
+    /**
+     * @var \Zenstruck\Bundle\ContentBundle\Repository\NodeRepository
+     */
+    protected $nodeRepository;
+
+    /**
+     * @param \Doctrine\ORM\EntityRepository $repository
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->pathRepository = $em->getRepository('ZenstruckContentBundle:Path');
+        $this->nodeRepository = $em->getRepository('ZenstruckContentBundle:Node');
+    }
+
+    /**
+     * @param \Zenstruck\Bundle\ContentBundle\Entity\Node $value
+     * @param \Symfony\Component\Validator\Constraint $constraint
+     * @return bool
+     */
+    public function isValid($value, Constraint $constraint)
+    {
+        $path = $value->getPrimaryPath();
+        if (null === $path) {
+
+            return true;
+        }
+        $conflicts = $this->pathRepository->findBy(array('uri' => $path->getUri()));
+        if (empty($conflicts)) {
+
+            return true;
+        }
+
+        /* @var \Zenstruck\Bundle\ContentBundle\Entity\Path $old */
+        $old = $value->getOldPrimaryPath();
+        if (!is_object($old)) {
+            $this->setMessage($constraint->message);
+            
+            return false;
+        }
+
+        foreach ($conflicts as $conflict) {
+            /* @var \Zenstruck\Bundle\ContentBundle\Entity\Path $conflict */
+            if (($conflict->getId() != $old->getId()) && $this->nodeRepository->findOneByPath($conflict)) {
+                $this->setMessage($constraint->message);
+                
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+}