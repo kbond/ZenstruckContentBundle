f22893c525160b96f48d81b18c2fae6d0834c5ec
kbond <kevinbond@gmail.com>
2011-08-11 10:14:43 -0400

    Fixed CTI listener to map subclasses of node

--- Listener/Doctrine/DiscriminatorListener.php
+++ Listener/Doctrine/DiscriminatorListener.php
@@ -16,26 +16,34 @@ class DiscriminatorListener implements EventSubscriber
 
 
     public function __construct($contentTypes)
-    {        
+    {
         $this->contentTypes = $contentTypes;
     }
 
 
-    public function getSubscribedEvents() 
-    {  
+    public function getSubscribedEvents()
+    {
         return array(Events::loadClassMetadata);
-    }  
-    
+    }
+
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
     {
         $classMetadata = $eventArgs->getClassMetadata();
+
+        // add subclasses to node
+        $subclasses = array_flip($this->contentTypes);
         
+        if ($classMetadata->name == $subclasses['node']) {
+            unset($subclasses['node']);
+            $classMetadata->subClasses = $subclasses;
+        }
+
         // check if class is defined in config
         if (isset($this->contentTypes[$classMetadata->name]))
         {
             // set discriminator
             $classMetadata->discriminatorMap = array_flip($this->contentTypes);
             $classMetadata->discriminatorValue = $this->contentTypes[$classMetadata->name];
-        }        
+        }
     }
 }
--- Repository/NodeRepository.php
+++ Repository/NodeRepository.php
@@ -4,20 +4,10 @@ namespace Zenstruck\Bundle\ContentBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
-use Doctrine\ORM\Query;
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
 class NodeRepository extends EntityRepository
 {
-    public function findOneByPath($uri, $refresh = true)
-    {
-        $node = $this->findOneBy(array('path' => $uri));
-        if ($refresh) {
-            $this->_em->refresh($node);
-        }
-
-        return $node;
-    }
 
 }
