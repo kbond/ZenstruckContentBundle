6777296e267b8d168f2a7b1f38854647f6c84dd0
kbond <kevinbond@gmail.com>
2011-11-03 17:23:18 -0400

    fixed copy/paste error

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the ZenstruckRedirectBundle package.
+ * This file is part of the ZenstruckContentBundle package.
  *
  * (c) Kevin Bond <http://zenstruck.com/>
  *
