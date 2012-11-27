0b60b66f41e086a450c69a6dae5d4310eba4b741
Kevin Bond <kevinbond@gmail.com>
2012-11-27 13:56:42 -0500

    removed unnecessary Entity class

--- Entity/Node.php
+++ Entity/Node.php
@@ -16,13 +16,82 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-abstract class Node extends Entity
+abstract class Node
 {
+    protected $id;
+
+    protected $title;
+
+    protected $path;
+
+    protected $updatedAt;
+
+    protected $createdAt;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function __toString()
+    {
+        return (string) $this->getTitle();
+    }
 
     /**
-     * @var string $path
+     * Returns the machine name of the class (without namespace)
      */
-    protected $path;
+    public function getContentType()
+    {
+        preg_match('#([\w]+)$#', get_class($this), $matches);
+        $className = $matches[1];
+
+        // camel case
+        $className = strtolower(preg_replace('#([a-z])([A-Z])#', '$1_$2', $className));
+
+        return $className;
+    }
 
     public function getPath()
     {
